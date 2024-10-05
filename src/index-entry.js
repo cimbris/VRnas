4f48c4e31d3efc2fbc4f9e5cf81459eea48f2098
Arseniy <arseniy.cimbris@gmail.com>
2024-10-05 14:09:13 +0300

    fix: fonts fixed

--- src/index-entry.js
+++ src/index-entry.js
@@ -1,3 +1,5 @@
 import "./index.html";
 import "./common-styles/base.scss";
 import "./common-scripts/header-submenu.js";
+import "./common-styles/hero.scss";
+import "./common-styles/preview.scss";
