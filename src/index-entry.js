bc5be51fbf9dc1b39767e43c7876bf766669314c
Arseniy <arseniy.cimbris@gmail.com>
2024-10-10 17:28:31 +0300

    feat: info btn shadow done

--- src/index-entry.js
+++ src/index-entry.js
@@ -3,3 +3,4 @@ import "./common-styles/base.scss";
 import "./common-scripts/header-submenu.js";
 import "./common-styles/hero.scss";
 import "./common-styles/preview.scss";
+import "./common-styles/info-btn.scss";
