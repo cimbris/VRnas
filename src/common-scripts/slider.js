3c4fa1907b2e925f00bf5163296c0c81f779e6bb
Arseniy <arseniy.cimbris@gmail.com>
2025-01-18 14:15:02 +0300

    feat: paginationWrapper script added

--- src/common-scripts/slider.js
+++ src/common-scripts/slider.js
@@ -1,20 +1,26 @@
 class Slider {
     sliderElement = null;
+    sliderAmount = 0;
 
     constructor() {}
 
     _initial(selector) {
         this.sliderElement = document.querySelector(selector);
+
         const wrapperHidden = this.#wrapperCreator();
         this.sliderElement.append(wrapperHidden);
+
         const { leftArrow, rightArrow } = this.#arrowsCreator();
         this.sliderElement.append(leftArrow, rightArrow);
-        console.log(this.sliderElement);
+
+        const paginationWrapper = this.#paginationCreator();
+        this.sliderElement.append(paginationWrapper);
     }
 
     #wrapperCreator() {
         const wrapperHidden = document.createElement("div");
         const lenta = document.createElement("div");
+        this.sliderAmount = this.sliderElement.children.length;
         lenta.append(...this.sliderElement.children);
         wrapperHidden.append(lenta);
         return wrapperHidden;
@@ -24,6 +30,17 @@ class Slider {
         const leftArrow = document.createElement("button");
         const rightArrow = document.createElement("button");
         return { leftArrow, rightArrow };
+    }
+
+    #paginationCreator() {
+        const paginationWrapper = document.createElement("div");
+
+        for (let i = 0; i < this.sliderAmount; i++) {
+            const paginationBtn = document.createElement("button");
+            paginationWrapper.append(paginationBtn);
+        }
+
+        return paginationWrapper;
     }
 }
 
